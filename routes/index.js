863f520a81833bd9a2ac3656f2847b7d7729e2be
Hee Jae Chae <poongnewga@gmail.com>
2017-10-19 10:27:32 +0900

    Changed Title again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HEEHAM' });
+  res.render('index', { title: 'HEEHAMs' });
 });
 
 module.exports = router;
